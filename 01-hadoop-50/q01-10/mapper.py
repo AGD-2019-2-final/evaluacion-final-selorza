d70dfe131d4ec9df81e1b754d8816c336dc25efc
Sebastián Elorza Velásquez <50224325+selorza@users.noreply.github.com>
2020-01-29 10:04:28 -0500

    Add files via upload
    Puntos solucionados

--- 01-hadoop-50/q01-10/mapper.py
+++ 01-hadoop-50/q01-10/mapper.py
@@ -2,3 +2,10 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        dato_columna = line.split(',')[2]
+        #valor = line.split(',')[4]
+        sys.stdout.write("{}\t1\n".format(dato_columna)) 
