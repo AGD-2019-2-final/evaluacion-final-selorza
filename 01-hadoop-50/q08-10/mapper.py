f2dad6590a8cf78648c15d29ff17ef2c32cc7752
Sebastián Elorza Velásquez <50224325+selorza@users.noreply.github.com>
2020-01-31 17:06:59 -0500

    Add files via upload

--- 01-hadoop-50/q08-10/mapper.py
+++ 01-hadoop-50/q08-10/mapper.py
@@ -3,3 +3,10 @@ import sys
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 
+if __name__=="__main__":
+	for linea in sys.stdin:
+		letra=linea.split("   ")[0]
+		valor=linea.split("   ")[2]
+		val=float(valor)
+
+		sys.stdout.write("{}\t{}\n".format(letra,val))
--- 01-hadoop-50/q08-10/reducer.py
+++ 01-hadoop-50/q08-10/reducer.py
@@ -1,4 +1,41 @@
 import sys
 #
-#  >>> Escriba el codigo del mapper a partir de este punto <<<
+# >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+if __name__ == '__main__':
+
+    curkey = None
+    suma = 0
+    n=0
+    promedio=0
+
+    ##
+    ## cada linea de texto recibida es una
+    ## entrada clave \tabulador valor
+    ##
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val=float(val)
+        
+
+        if key == curkey:
+            suma = suma + val
+            n += 1
+            promedio = suma / n
+
+        else:
+            if curkey is None:
+            	curkey = key
+            	suma = val
+            	n = 1
+            	promedio = suma / n
+            else:
+	            sys.stdout.write("{}\t{}\t{}\n".format(curkey, suma,promedio))
+	            curkey = key
+	            suma = val
+	            n = 1
+	            promedio = suma / n
+
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, suma,promedio))
