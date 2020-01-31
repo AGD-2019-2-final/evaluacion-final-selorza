44be44b55e113162ee99a14acc26a2263aebc400
Sebastián Elorza Velásquez <50224325+selorza@users.noreply.github.com>
2020-01-31 17:07:26 -0500

    Add files via upload

--- 01-hadoop-50/q09-10/mapper.py
+++ 01-hadoop-50/q09-10/mapper.py
@@ -2,3 +2,14 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+if __name__=="__main__":
+	for linea in sys.stdin:
+		letra=linea.split("   ")[0]
+		fecha=linea.split("   ")[1]
+		valor=linea.split("   ")[2]
+
+		valor=int(valor)
+		valor=str(valor)
+		valor=valor.zfill(3)
+		sys.stdout.write("{}\t{}\t{}\t{}\n".format(valor,letra,fecha,int(valor)))
--- 01-hadoop-50/q09-10/reducer.py
+++ 01-hadoop-50/q09-10/reducer.py
@@ -1,4 +1,22 @@
 import sys
 #
-#  >>> Escriba el codigo del mapper a partir de este punto <<<
+# >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+if __name__ == '__main__':
+
+    curkey = None
+    total = 0
+    n=0
+    ##
+    ## cada linea de texto recibida es una
+    ## entrada clave \tabulador valor
+    ##
+    for line in sys.stdin:
+    	if n<6:
+	        texto,key,fecha,val = line.split("\t")
+	        val = int(val)
+	        sys.stdout.write("{}\t{}\t{}\n".format(key, fecha,val))
+	        n+=1
+
+    #sys.stdout.write("{},{}\n".format(curkey, total))
