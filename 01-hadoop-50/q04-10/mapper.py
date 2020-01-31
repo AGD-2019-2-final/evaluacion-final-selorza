b4984eec74a121b0bcd1165767c26f367ca81a55
Sebastián Elorza Velásquez <50224325+selorza@users.noreply.github.com>
2020-01-31 16:51:41 -0500

    Add files via upload

--- 01-hadoop-50/q04-10/mapper.py
+++ 01-hadoop-50/q04-10/mapper.py
@@ -2,4 +2,7 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-
+if __name__ == "__main__":
+    for line in sys.stdin:
+        letra = line.split('   ')[0]
+        sys.stdout.write("{}\t1\n".format(letra))
